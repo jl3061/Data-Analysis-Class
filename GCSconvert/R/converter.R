96e6296f76bc652946db8524e8b67f2f3042376c
jl3061 <jl3061@georgetown.edu>
2023-10-17 16:23:34 -0400

    Assignment 3 Package

--- GCSconvert/R/converter.R
+++ GCSconvert/R/converter.R
@@ -1,11 +1,48 @@
 #'@title Converter
 #'@param Lon longitudes to be converted
 #'@param Lat latitudes to be converted
+#'@param Date change dates to YYYY-MM-DD format
 #'@export
 
-latlonconverter <- function(Lon, Lat) {
-  parse_lon(Lon)
-  parse_lat(Lat)
+converter <- function(lon, lat, date) {
   
-  return(df)
+  if (missing(date)){
+    
+    degree_lon <- sapply(strsplit(data[[lon]], "°"), "[[", 1)
+    degree_lon <- as.numeric(gsub("°", "", degree_lon))
+    minute_lon <- sapply(strsplit(data[[lon]], "°"), "[[", 2)
+    minute_lon <- as.numeric(gsub("'W", "", minute_lon))/60
+    
+    new.lon <- -degree_lon - minute_lon
+    
+    degree_lat <- sapply(strsplit(lat, "°"), "[[", 1)
+    degree_lat <- as.numeric(gsub("°", "", degree_lon))
+    minute_lat <- sapply(strsplit(lat, "°"), "[[", 2)
+    minute_lat <- as.numeric(gsub("'N", "", minute_lon))/60
+    
+    new.lat <- -degree_lat - minute_lat
+    
+    new_data <- data.frame(new.lon, new.lat)
+    
+  }else{
+    
+    degree_lon <- sapply(strsplit(lon, "°"), "[[", 1)
+    degree_lon <- as.numeric(gsub("°", "", degree_lon))
+    minute_lon <- sapply(strsplit(lon, "°"), "[[", 2)
+    minute_lon <- as.numeric(gsub("'W", "", minute_lon))/60
+    
+    new.lon <- -degree_lon - minute_lon
+    
+    degree_lat <- sapply(strsplit(lat, "°"), "[[", 1)
+    degree_lat <- as.numeric(gsub("°", "", degree_lon))
+    minute_lat <- sapply(strsplit(lat, "°"), "[[", 2)
+    minute_lat <- as.numeric(gsub("'W", "", minute_lon))/60
+    
+    new.lat <- -degree_lat - minute_lat
+    
+    new.date <- as.Date(date, "%d-%B-%y")
+    
+    new_data <- data.frame(new.lon, new.lat, new.date)
+  }
+  return(new_data)
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data-Analysis-Class
+
+GCSconvert is a package used to change the longitude and latitude to decimal degrees from minutes.
